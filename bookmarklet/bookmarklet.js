056eb90115d21fc235f90e142af407ede9beb9fe
Nick Spacek <nick.spacek@gmail.com>
2010-01-07 07:17:43 -0400

    Initial commit.

---
+++ bookmarklet/bookmarklet.js
@@ -0,0 +1,1 @@
+(function(){s=document.createElement('script');s.type='text/javascript';s.src='http://nick.ci-fi.net/google-calendar-print.js';document.getElementsByTagName('head')[0].appendChild(s);})();
